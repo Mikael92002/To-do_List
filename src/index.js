ecd3dde77cedb0c111545edb88806102096b8ae1
Mikael <174565360+Mikael92002@users.noreply.github.com>
2025-04-02 21:15:16 -0500

    Notebook line creating logic implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import "./style.css";
 import { displayLogic } from "./logic";
-import { ToDoLogic } from "./logic";
--- src/logic.js
+++ src/logic.js
@@ -8,22 +8,8 @@ const displayLogic = (function () {
     const dialog = document.querySelector("dialog");
 
     newProjectButton.addEventListener("click", () => {
-        //wipe screen:
-        if (ToDoDivsArray.length > 0) {
-            removeChildren(notebookContainer,
-                ToDoDivsArray[ToDoDivsArray.length - 1].getHeader(),
-                ToDoDivsArray[ToDoDivsArray.length - 1].getNotebook());
-        }
         //show dialog:
         dialog.showModal();
-        //append new items:
-        const headerDiv = document.createElement("header");
-        headerDiv.id = "header";
-        const notebook = document.createElement("div");
-        notebook.id = "notebook";
-        let ToDoDiv = new ToDoDivs(headerDiv, notebook);
-        ToDoDivsArray.push(ToDoDiv);
-        notebookContainer.append(headerDiv, notebook);
     });
 
     //class to store header and notebook divs, object gets stored in ToDoDivsArray:
@@ -49,23 +35,56 @@ const displayLogic = (function () {
         })
     };
 
-})();
 
-const ToDoLogic = (function () {
     const ToDoArray = [];
     const confirm = document.querySelector("#submit");
     const title = document.querySelector("#title");
     const description = document.querySelector("#description");
-    const priority = document.querySelector(".low");
+    const low = document.querySelector(".low");
     const date = document.querySelector("#date");
-    const dialog = document.querySelector("dialog");
 
     confirm.addEventListener("click", () =>{
-        let ToDoObject = new ToDo(title.value, description.value, date.value, priority.textContent);
+        //wipe screen:
+        if (ToDoDivsArray.length > 0) {
+            removeChildren(notebookContainer,
+                ToDoDivsArray[ToDoDivsArray.length - 1].getHeader(),
+                ToDoDivsArray[ToDoDivsArray.length - 1].getNotebook());
+        };
+        //append new items:
+        //header:
+        const headerDiv = document.createElement("header");
+        headerDiv.id = "header";
+
+        //notebook div:
+        const notebook = document.createElement("div");
+        notebook.id = "notebook";
+        for(let i = 0;i<20;i++){
+            const divPosition = (i)/20 * 100;
+            console.log(divPosition);
+            const lineDiv = document.createElement("div");
+            lineDiv.style.position = "relative";
+            lineDiv.style.top = `${divPosition}%`;
+            lineDiv.style.width = "100%";
+            lineDiv.style.height = "0.5px";
+            lineDiv.style.backgroundColor = "black";
+            notebook.append(lineDiv);
+        }
+
+
+        let ToDoDiv = new ToDoDivs(headerDiv, notebook);
+        ToDoDivsArray.push(ToDoDiv);
+        notebookContainer.append(headerDiv, notebook);
+        if(title.value.trim() === ""){
+            title.value = "No title";
+        }
+        let ToDoObject = new ToDo(title.value, description.value, date.value, low.textContent);
         ToDoArray.push(ToDoObject);
         for(let i = 0;i<ToDoArray.length;i++){
             console.log(ToDoArray[i]);
         }
+        title.value = "";
+        description.value = "";
+        date.value="";
         dialog.close();
     });
 
@@ -109,5 +128,4 @@ const ToDoLogic = (function () {
 
 
 
-export { ToDoLogic };
 export { displayLogic };
